6f490f060076d196e8fb5c29dfc6dd85690520a4
Larissa Gusmao <larissamgusmao@gmail.com>
2020-04-23 19:31:41 -0400

    Updates

---
+++ precache-manifest.ee4028f6247a48598c2a93aedae1fb80.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ecb6f1b9fd7912d4c6ea35b9370bcef",
+    "url": "/weather_finder/index.html"
+  },
+  {
+    "revision": "368b879357fcc54837c0",
+    "url": "/weather_finder/static/css/2.1b627c76.chunk.css"
+  },
+  {
+    "revision": "0038c943bb8cd2627626",
+    "url": "/weather_finder/static/css/main.a12bd6c6.chunk.css"
+  },
+  {
+    "revision": "368b879357fcc54837c0",
+    "url": "/weather_finder/static/js/2.96b28168.chunk.js"
+  },
+  {
+    "revision": "847d86450e191106adb5af2d19aad798",
+    "url": "/weather_finder/static/js/2.96b28168.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0038c943bb8cd2627626",
+    "url": "/weather_finder/static/js/main.27302c27.chunk.js"
+  },
+  {
+    "revision": "474a7da74c91e959786e",
+    "url": "/weather_finder/static/js/runtime-main.050199a5.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/weather_finder/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/weather_finder/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/weather_finder/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/weather_finder/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/weather_finder/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/weather_finder/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
