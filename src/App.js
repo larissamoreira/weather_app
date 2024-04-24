1ed8a4c479f63108d1813959883f2f4e498003c1
Larissa Gusmao <larissamgusmao@gmail.com>
2024-04-24 14:03:23 -0400

    fix columns

--- src/App.js
+++ src/App.js
@@ -49,10 +49,10 @@ class App extends React.Component {
                     <div className="main">
                         <div className="container">
                             <div className="row">
-                                <div className="col-xs-5 title-container">
+                                <div className="col title-container">
                                     <Titles />
                                 </div>
-                                <div className="col-xs-7 form-container">
+                                <div className="col form-container">
                                     <Form getWeather={this.getWeather} />
                                     <Weather
                                         temperature={this.state.temperature}
