9427270bc0fd34db158c1916506f7b6bbe0e1f14
Larissa Gusmao <larissamgusmao@gmail.com>
2024-04-29 12:49:35 -0400

    add build

---
+++ build/precache-manifest.d1ca06cde54b5b4dabb07acb95deb5a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2984756d71df8ef08b54ec593d62cd7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36ab7205002aa418b90d",
+    "url": "/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "0f4d15bdc829a7a12fbc",
+    "url": "/static/css/main.3a8350fe.chunk.css"
+  },
+  {
+    "revision": "36ab7205002aa418b90d",
+    "url": "/static/js/2.070a5080.chunk.js"
+  },
+  {
+    "revision": "0f4d15bdc829a7a12fbc",
+    "url": "/static/js/main.15f26a1d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/static/media/bg.b6796c73.jpg"
+  }
+]);
